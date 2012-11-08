27b6c1f6512acb064cae9546c11bed7b5578a83a
sergey <sergey.zelenin@gmail.com>
2012-11-08 14:01:55 +0200

    Csharp client on Nancy

---
+++ tetris-servers/csharp/README.md
@@ -0,0 +1,13 @@
+Welcome
+=======
+It is a testris template client project based on Nancy (http://www.nancyfx.org/).
+
+
+Getting started 
+---------------
+* Install NuGet (from http://nuget.org/)
+* Open the Solution in Visual Studio 2010 (at least Visual Studio Web Developer 2010 Express is required)
+* To get Nancy
+  * Go to Tool | Library Package Manager | Package Manager Console
+  * Run: Install-Package Nancy.Hosting.Self
+* Enjoy!  
---
+++ tetris-servers/csharp/nancy/Module.cs
@@ -0,0 +1,26 @@
+﻿namespace NancySelfHosting
+{
+    using Nancy;
+    using System;
+    using System.Diagnostics;
+
+    public class Module : NancyModule
+    {
+        public Module()
+        {
+            Get["/"] = parameters => { return answer(parameters); };
+        }
+
+        private String answer(dynamic parameters)
+        {
+            String query = "";
+            foreach (var name in Request.Query)
+                query += name + ": " + Request.Query[name] + " ";
+
+            Debug.WriteLine(query);
+
+            //add "drop" to response when you need to drop a figure
+            return "left=0, right=0, rotate=0";
+        }
+    }
+}
