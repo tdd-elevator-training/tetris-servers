d3aa886ba0f1f1e71b7a6528f919d6af479fb5e1
sergey <sergey.zelenin@gmail.com>
2012-11-13 19:27:50 +0200

    Changed provided packaging

--- tetris-servers/java/src/main/java/TetrisServlet.java
+++ tetris-servers/java/src/main/java/TetrisServlet.java
@@ -2,7 +2,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 
 public class TetrisServlet extends HttpServlet {
