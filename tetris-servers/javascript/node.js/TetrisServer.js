0da9223410d3e8f6a0999f249c15cc023ca84474
Oleksandr Baglai <apofig@gmail.com>
2013-09-05 14:44:15 +0300

    Исправил node.js клиента

--- tetris-servers/javascript/node.js/TetrisServer.js
+++ tetris-servers/javascript/node.js/TetrisServer.js
@@ -2,9 +2,9 @@ var log = function(string) {
     console.log(string);
 };
 
-var util = require('util');
-
 var hostIp = '127.0.0.1';
+//var hostIp = 'tetrisj.jvmhost.net';
+
 var userName = 'apofig';
 var protocol = 'WS';
 
@@ -15,15 +15,21 @@ if (protocol == 'HTTP') {
     http.createServer(function (request, response) {
         var p = url.parse(request.url, true).query;
 
-        var answer = answer(p.figure, p.x, p.y, p.glass, p.next);
+        var result = answer(p.figure, p.x, p.y, p.glass, p.next);
+        log("submitted: " + result);
 
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end(answer);
+        response.end(result);
     }).listen(8888, hostIp);
 
     log('Server running at http://' + hostIp + ':8888/');
 } else {
-    var server = 'ws://tetrisj.jvmhost.net:12270/tetris-contest/ws';
+    var port = 8080;
+    if (hostIp == 'tetrisj.jvmhost.net') {
+        port = 12270;
+    }
+
+    var server = 'ws://' + hostIp + ':' + port + '/tetris-contest/ws';
     var WebSocket = require('ws');
     var ws = new WebSocket(server + '?user=' + userName);
 
@@ -36,17 +42,18 @@ if (protocol == 'HTTP') {
     });
 
     ws.on('message', function(message) {
-        log('received: %s', message);
+        log('received: ' + message);
 
-        var pattern = new RegExp(/^figure=(\\w+)&x=(\\d+)&y=(\\d+)&glass=(.*)&next=(\\w*)$/);
+        var pattern = new RegExp(/^figure=(\w+)&x=(\d+)&y=(\d+)&glass=(.*)&next=(\w*)$/);
         var parameters = message.match(pattern);
 
-        var answer = answer(parameters[1], parameters[2], parameters[3], parameters[4], parameters[5]);
+        var result = answer(parameters[1], parameters[2], parameters[3], parameters[4], parameters[5]);
+        log("submitted: " + result);
 
-        ws.send(answer);
+        ws.send(result);
     });
 
-    log('Web socket client running at http://' + hostIp + ':8080/');
+    log('Web socket client running at ' + server);
 }
 
 var Element = {         // из этих символов состоит строка glass
@@ -63,5 +70,5 @@ var ROTATE_90_COUNTERCLOCKWISE = 3; // повернуть против часо�
 function answer(figure, x, y, glass, next) {
 	// add "drop" to response when you need to drop a figure
 	// for details please check http://codenjoy.com/portal/?p=170#commands
-	return "left=0, right=0, rotate=0";
+	return "left=0, right=0, rotate=0, drop";
 }
